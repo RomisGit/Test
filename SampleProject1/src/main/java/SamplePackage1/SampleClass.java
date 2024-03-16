cbc6fc082be1f4fcf5995422697969072f22dff2
romit <romit@192.168.0.212>
2024-03-16 22:24:39 +0000

    Test

---
+++ SampleProject1/src/main/java/SamplePackage1/SampleClass.java
@@ -0,0 +1,11 @@
+package SamplePackage1;
+
+public class SampleClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello. Romi...");
+
+	}
+
+}
